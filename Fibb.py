6eb3309ec9d60416f9280f2ebf9cb6a09723d686
HiggloH <hilding.hammarback@gmail.com>
2023-12-08 17:22:04 +0100

    Nu funkar alla längder på sekvensen

--- Fibb.py
+++ Fibb.py
@@ -2,18 +2,22 @@ from terminaltables import SingleTable
 
 running = True
 
-# Eftersom sekvensen alltid startar på 0 och 1
-talen = [0, 1]
-header = ["F:0", "F:1"]
+talen = []
+header = []
 
 
 def generera_tal(antal):
     # Formeln för sekvensen: Talen[n] = Talen[n-1] + Talen[n-2]
 
     # Range(2, antal tal) är för att inte starta på index 0 i talen listan
-    for n in range(2, antal):
-        nya_talet = talen[n - 1] + talen[n - 2]
-        talen.append(nya_talet)
+    for n in range(antal):
+        if n == 0:
+            talen.append(0)
+        elif n == 1:
+            talen.append(1)
+        else:
+            nya_talet = talen[n - 1] + talen[n - 2]
+            talen.append(nya_talet)
 
         header.append('F:{0}'.format(n))
 
@@ -47,5 +51,5 @@ while running:
         running = False
     else:
         # Återställ dessa listor för att inte förstöra sekvensen och huvudet till tabellen
-        talen = [0, 1]
-        header = ["F:0", "F:1"]
+        talen = []
+        header = []
